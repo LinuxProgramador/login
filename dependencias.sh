5b9d9e4d1f966f807df971ccc1407f197d16a3a0
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-06-04 13:18:13 -0600

    Add files via upload

---
+++ dependencias.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+path=$(pwd | cut -f1)
+
+function dependencies {
+
+ apt install neofetch -y
+ clear
+
+ read -p "Ingrese la password a utilizar en su login: " pass
+ read -p "Repita su password: " pass_validation
+ if [ "$pass" = "$pass_validation" ] ; then
+
+  echo $pass | shasum -a 512 | base64 > /home/.password
+  pass=""
+  pass_validation=""
+
+  chmod 600 /home/.password
+  chmod 700 $path/login.sh
+
+  clear
+  sleep 1
+
+  echo "Nota: copiar el login.sh a binarios con (cp login.sh /usr/bin/)."
+  echo "Modificar el archivo bash.bashrc en el directorio (/etc/)"
+  echo "Para abrir siempre el login cuando acceda a userland "
+
+ else
+    echo "password incorrecta!"
+    exit
+
+ fi
+}
+dependencies
---
+++ login.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+#Login en bash para userland
+#Elaborado por WhiteHack
+
+Exit=$(ps | grep bash | cut -d'p' -f1 )
+password_local=$(cat /home/.password | base64 -d | cut -f1)
+user=$(whoami | cut -f1)
+
+function login {
+
+
+  clear
+  neofetch
+
+  for _ in 1 2 3 4 5 6 7 8 9 10 ; do
+
+     read -p "ingrese su password: " pass
+     pass=$(echo $pass | shasum -a 512 | cut -f1)
+
+     if [ "$pass" = "$password_local" ] ; then
+        echo "acceso consedido!"
+        exit
+
+     else
+         echo "password incorrecta!"
+     fi
+
+  done
+  clear
+  echo "Saliendo de userland!"
+  sleep 1
+  kill -9 $Exit
+
+}
+
+
+function main {
+
+   trap "clear && echo 'Opcion invalida!' && sleep 1 &&  kill -9 $Exit" 2 20
+
+   if [ "$user" != 'root'  ] ; then
+         login
+
+   fi
+
+}
+main
+
